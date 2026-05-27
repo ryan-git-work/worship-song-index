31a44f46af8f1edde1cbdbfceeacd7f3d7b0f178
Ryan Loche <ryanloche@Ryans-MacBook-Pro-16.local>
2026-05-27 12:23:52 -0500

    Build songs from committed chunks

---
+++ src/lib/songs.ts
@@ -0,0 +1,49 @@
+import type { Song } from '../types/song';
+
+import chunk1 from '../../public/songs-chunk-1.json';
+import chunk2 from '../../public/songs-chunk-2.json';
+import chunk3 from '../../public/songs-chunk-3.json';
+import chunk4 from '../../public/songs-chunk-4.json';
+import chunk5 from '../../public/songs-chunk-5.json';
+import chunk6 from '../../public/songs-chunk-6.json';
+import chunk7 from '../../public/songs-chunk-7.json';
+import chunk81 from '../../public/songs-chunk-8-1.json';
+import chunk82 from '../../public/songs-chunk-8-2.json';
+import chunk9 from '../../public/songs-chunk-9.json';
+import chunk10 from '../../public/songs-chunk-10.json';
+import chunk111 from '../../public/songs-chunk-11-1.json';
+import chunk112 from '../../public/songs-chunk-11-2.json';
+import chunk12 from '../../public/songs-chunk-12.json';
+import chunk13 from '../../public/songs-chunk-13.json';
+import chunk141 from '../../public/songs-chunk-14-1.json';
+import chunk142 from '../../public/songs-chunk-14-2.json';
+import chunk15 from '../../public/songs-chunk-15.json';
+import chunk16 from '../../public/songs-chunk-16.json';
+import chunk171 from '../../public/songs-chunk-17-1.json';
+import chunk172 from '../../public/songs-chunk-17-2.json';
+import chunk18 from '../../public/songs-chunk-18.json';
+
+export const songs: Song[] = [
+  ...chunk1,
+  ...chunk2,
+  ...chunk3,
+  ...chunk4,
+  ...chunk5,
+  ...chunk6,
+  ...chunk7,
+  ...chunk81,
+  ...chunk82,
+  ...chunk9,
+  ...chunk10,
+  ...chunk111,
+  ...chunk112,
+  ...chunk12,
+  ...chunk13,
+  ...chunk141,
+  ...chunk142,
+  ...chunk15,
+  ...chunk16,
+  ...chunk171,
+  ...chunk172,
+  ...chunk18,
+] as Song[];
